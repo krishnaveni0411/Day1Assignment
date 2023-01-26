633340258d3970e81aedb9a0c13544d641ecea35
krishnaveni k <64397669+krishnaveni0411@users.noreply.github.com>
2023-01-26 12:34:46 +0530

    Add files via upload

---
+++ assigment2/Mobile.java
@@ -0,0 +1,19 @@
+package week1.assigment2;
+
+public class Mobile {
+	public void sendMessage() {
+		System.out.println("Send Message");
+	}
+	public void shareDocument() {
+		System.out.println("Share Document");
+	}
+	public void call() {
+		System.out.println("Call");
+	}
+public static void main(String[] args) {
+	Mobile B=new Mobile();
+	B.sendMessage();
+	B.shareDocument();
+	B.call();
+}
+}
